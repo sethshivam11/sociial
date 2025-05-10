c6cb75d2fd87f0cbb18f32ae1d6ce196b037f0d3
Shivam Soni <130232913+sethshivam11@users.noreply.github.com>
2025-05-10 11:58:35 +0000

    Added dynamic loading to homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,15 @@
-import LandingNav from "@/components/LandingNav";
-import HeroSection from "@/components/HeroSection";
-import BentoGrid from "@/components/BentoGrid";
-import FAQs from "@/components/FAQs";
-import WobbleCardDemo from "@/components/WobbleCardDemo";
-import Typewriter from "@/components/Typewriter";
-import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const LandingNav = dynamic(() => import("../../components/LandingNav"));
+const HeroSection = dynamic(() => import("../../components/HeroSection"));
+const BentoGrid = dynamic(() => import("../../components/BentoGrid"));
+const FAQs = dynamic(() => import("../../components/FAQs"));
+const WobbleCardDemo = dynamic(() => import("../../components/WobbleCardDemo"));
+const Typewriter = dynamic(() => import("../../components/Typewriter"));
+const Footer = dynamic(() => import("../../components/Footer"));
 
 export default function Page() {
+
   return (
     <div className="flex flex-col items-center overflow-hidden col-span-10">
       <LandingNav />
