d9714c25fdda0b332871dbd7624e8b9d632d1fa1
Shivam Soni <130232913+sethshivam11@users.noreply.github.com>
2026-07-11 05:56:21 +0000

    Allowed sitemap in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/home", "/sign-in", "/sign-up", "/terms", "/privacy"],
+      allow: ["/home", "/sign-in", "/sign-up", "/terms", "/privacy", "/sitemap.xml"],
       disallow: ["/"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_LINK}/sitemap.xml`,
